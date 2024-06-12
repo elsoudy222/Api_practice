1240bf73b8d20216ba3bd53be5b95d113be94a67
soudyjr <elsoudy222@gmail.com>
2024-06-12 13:58:59 +0400

    Refactor Api State with Freezed

--- lib/presentation/manager/api_state.dart
+++ lib/presentation/manager/api_state.dart
@@ -1,27 +1,41 @@
-part of 'api_cubit.dart';
+// part of 'api_cubit.dart';
+//
+// @immutable
+// sealed class ApiState {}
+//
+// class ApiInitial extends ApiState {}
+//
+// class SuccessGetAllUsers extends ApiState {
+//
+//   final List<Users> users;
+//   SuccessGetAllUsers(this.users);
+// }
+//
+// class SuccessGetUserById extends ApiState {
+//
+//   final Users userDetails;
+//   SuccessGetUserById(this.userDetails);
+// }
+// class SuccessCreateNewUser extends ApiState {
+//
+//   final Users newUser;
+//   SuccessCreateNewUser(this.newUser);
+// }
+// class SuccessDeleteUser extends ApiState {
+//   final dynamic data;
+//   SuccessDeleteUser(this.data);
+// }
 
-@immutable
-sealed class ApiState {}
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class ApiInitial extends ApiState {}
+import '../../core/exeptions/network_exeptions/network_exceptions.dart';
 
-class SuccessGetAllUsers extends ApiState {
+part 'api_state.freezed.dart';
 
-  final List<Users> users;
-  SuccessGetAllUsers(this.users);
-}
-
-class SuccessGetUserById extends ApiState {
-
-  final Users userDetails;
-  SuccessGetUserById(this.userDetails);
-}
-class SuccessCreateNewUser extends ApiState {
-
-  final Users newUser;
-  SuccessCreateNewUser(this.newUser);
-}
-class SuccessDeleteUser extends ApiState {
-  final dynamic data;
-  SuccessDeleteUser(this.data);
+@freezed
+class ApiState<T> with _$ApiState<T>{
+ const factory ApiState.initial() = Initial<T>;
+ const factory ApiState.loading() = Loading<T>;
+ const factory ApiState.success(T data) = Success<T>;
+ const factory ApiState.failure(NetworkExceptions networkExceptions) = Failure<T>;
 }
